336e5f0094c5d9bcb9c765ad872949372e45580d
Andrey Gorlov <andgor86@yandex.ru>
2026-01-18 11:50:14 +0300

    upd main

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,9 +2,17 @@ import pytest
 
 from src.main import Product
 
+
 @pytest.fixture()
 def product_1():
     return Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
+
+
 @pytest.fixture()
 def product_2():
     return Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
+
+
+@pytest.fixture()
+def product_3():
+    return Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14)
