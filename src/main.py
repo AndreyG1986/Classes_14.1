b96fdf5d84dbf174d126f19e4a1dd7578773b941
Andrey Gorlov <andgor86@yandex.ru>
2026-03-02 15:30:01 +0300

    fix main

--- src/main.py
+++ src/main.py
@@ -128,6 +128,26 @@ class Category:
         return self.__products.copy()  # Возвращаем копию для безопасности
 
 
+class Smartphone(Product):
+    # Переопределяем метод базового класса
+    def __init__(self, name, description, price, quantity, efficiency, model, memory, color):
+        # Вызываем метод базового класса
+        super().__init__(name, description, price, quantity)
+        self.efficiency =efficiency
+        self.model = model
+        self.memory = memory
+        self.color = color
+
+
+class LawnGrass(Product):
+    # Переопределяем метод базового класса
+    def __init__(self, name, description, price, quantity, country, germination_period, color):
+        # Вызываем метод базового класса
+        super().__init__(name, description, price, quantity)
+        self.country = country
+        self.germination_period = germination_period
+        self.color = color
+
 if __name__ == '__main__':
     print("\n=== Задание 16.1 Наследование===")
     smartphone1 = Smartphone("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5, 95.5,
@@ -181,31 +201,31 @@ if __name__ == '__main__':
     print(grass2.germination_period)
     print(grass2.color)
 
-    smartphone_sum = smartphone1 + smartphone2
-    print(smartphone_sum)
-
-    grass_sum = grass1 + grass2
-    print(grass_sum)
-
-    try:
-        invalid_sum = smartphone1 + grass1
-    except TypeError:
-        print("Возникла ошибка TypeError при попытке сложения")
-    else:
-        print("Не возникла ошибка TypeError при попытке сложения")
-
-    category_smartphones = Category("Смартфоны", "Высокотехнологичные смартфоны", [smartphone1, smartphone2])
-    category_grass = Category("Газонная трава", "Различные виды газонной травы", [grass1, grass2])
-
-    category_smartphones.add_product(smartphone3)
-
-    print(category_smartphones.products)
-
-    print(Category.product_count)
-
-    try:
-        category_smartphones.add_product("Not a product")
-    except TypeError:
-        print("Возникла ошибка TypeError при добавлении не продукта")
-    else:
-        print("Не возникла ошибка TypeError при добавлении не продукта")
+    # smartphone_sum = smartphone1 + smartphone2
+    # print(smartphone_sum)
+    #
+    # grass_sum = grass1 + grass2
+    # print(grass_sum)
+    #
+    # try:
+    #     invalid_sum = smartphone1 + grass1
+    # except TypeError:
+    #     print("Возникла ошибка TypeError при попытке сложения")
+    # else:
+    #     print("Не возникла ошибка TypeError при попытке сложения")
+    #
+    # category_smartphones = Category("Смартфоны", "Высокотехнологичные смартфоны", [smartphone1, smartphone2])
+    # category_grass = Category("Газонная трава", "Различные виды газонной травы", [grass1, grass2])
+    #
+    # category_smartphones.add_product(smartphone3)
+    #
+    # print(category_smartphones.products)
+    #
+    # print(Category.product_count)
+    #
+    # try:
+    #     category_smartphones.add_product("Not a product")
+    # except TypeError:
+    #     print("Возникла ошибка TypeError при добавлении не продукта")
+    # else:
+    #     print("Не возникла ошибка TypeError при добавлении не продукта")
